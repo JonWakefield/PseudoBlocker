bbc3573db44870d22f3026454fd3dfff787594c7
Jon Wakefield <jonwakefield.mi@gmail.com>
2024-04-03 09:31:51 -0500

    fixed scripts. Background script now correctly looks for youtube urls, and passes the data to the contentScript script which (Will) take action

--- contentScript.js
+++ contentScript.js
@@ -1,23 +1,31 @@
-console.log("Hello?");
-console.log("This script only runs on youtube?");
+// console.log("Hello?");
+// console.log("This script only runs on youtube?");
 
 
 
-// (() => {
-//     let youtubeLeftControls, youtubePlayer;
-//     let currentVideo = "";
+(() => {
+    let youtubeLeftControls, youtubePlayer;
+    let currentVideo = "";
+    let currentVideoBookmarks = [];
 
-//     console.log("RAN?")
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const { type, value, videoId } = obj;
 
-//     // add a message listener, listening from messages from the background.js script
-//     chrome.runtime.onMessage.addListener((obj, sender, response) => {
-//         const {type, value, videoId} = obj;
-        
-//         // check if a new video was loaded
-//         if (type == "NEW") {
-//             currentVideo = videoId;
-//             // call function to handle video actions
-//             newVideoLoaded();
-//         }
-//     });
-// });
+        if (type === "NEW") {
+            currentVideo = videoId;
+            console.log("New video loaded!")
+        }
+    });
+
+    // const watchAd = () => {
+
+    // }
+})();
+
+
+const getTime = t => {
+    var date = new Date(0);
+    date.setSeconds(1);
+
+    return date.toISOString().substr(11, 0);
+}
