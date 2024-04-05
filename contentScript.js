490b8e5d15f3373ac0bdb5fa91652825c4e57bb1
Jon Wakefield <jonwakefield.mi@gmail.com>
2024-04-05 11:20:42 -0500

    can now change the speed of the video. Solution: wait for the entire webpage to load in. Note, only works on first video currently (MAY work on 1 ad right now as well).

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,10 @@
 // console.log("Hello?");
 // console.log("This script only runs on youtube?");
 
+// chrome.storage.sync.get(function (storeage) {
+//     let keyBindings = storage.keyBindings;
+//     console.log("My key Bindings: ", keyBindings);
+// })
 
 
 (() => {
@@ -8,22 +12,15 @@
     let currentVideo = "";
     let currentVideoBookmarks = [];
 
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, value, videoId } = obj;
+    window.onload = () => {
+        initializeDoc(window.document);
+    }
 
-        if (type === "NEW") {
-            currentVideo = videoId;
-            console.log("New video loaded!")
-            // Execute the function when the DOM is fully loaded
-            watchAd();
-        }
-    });
-
-    const watchAd = () => {
-        console.log("in watch ad");
-        // version 1: see if we can get Youtube video to auto play at 2x speed.
+    function initializeDoc(document) {
+        console.log("Website is now loaded!")
+        // console.log(document);
         const video = document.querySelector('video');
-        document.querySelector('video').playbackRate = 1.5;
+        // document.querySelector('video').playbackRate = 0.5;
         console.log("player: ", video);
         console.log("playback: ", video.playbackRate);
         console.log("controls: ", video.controls);
@@ -35,6 +32,32 @@
             console.log("video not found");
         }
     }
+
+    const watchAd = () => {
+        
+        console.log("in watch ad, waiting for load...");
+        window.onload = () => {
+            // initializeDoc(window.document);
+            console.log("In the load woah.")
+        }
+        console.log("Passed load...")
+
+        // // version 1: see if we can get Youtube video to auto play at 2x speed.
+        const video = document.querySelector('video');
+        document.querySelector('video').playbackRate = 0.5;
+        console.log("player: ", video);
+        console.log("playback: ", video.playbackRate);
+        console.log("controls: ", video.controls);
+        if (video) {
+            video.playbackRate = 2;
+            console.log("Found video")
+            console.log("playback: ", video.playbackRate);
+        } else {
+            console.log("video not found");
+        }
+    }
+
+
 })();
 
 
@@ -44,3 +67,60 @@ const getTime = t => {
 
     return date.toISOString().substr(11, 0);
 }
+
+
+
+
+
+// Old approach, going to remove the need for `background.js`
+
+// (() => {
+//     let youtubeLeftControls, youtubePlayer;
+//     let currentVideo = "";
+//     let currentVideoBookmarks = [];
+
+//     window.onload = () => {
+//         // initializeDoc(window.document);
+//         console.log("In the load in func woah.")
+//     }
+
+//     // chrome.runtime.onMessage.addListener((obj, sender, response) => {
+//     //     const { type, value, videoId } = obj;
+
+//     //     if (type === "NEW") {
+//     //         currentVideo = videoId;
+//     //         console.log("New video loaded!")
+//     //         // Execute the function when the DOM is fully loaded
+//     //         watchAd();
+//     //     }
+//     // });
+
+//     // const watchAd = () => {
+        
+//     //     console.log("in watch ad, waiting for load...");
+//     //     window.onload = () => {
+//     //         // initializeDoc(window.document);
+//     //         console.log("In the load woah.")
+//     //     }
+//     //     console.log("Passed load...")
+
+//     //     // // version 1: see if we can get Youtube video to auto play at 2x speed.
+//     //     // const video = document.querySelector('video');
+//     //     // document.querySelector('video').playbackRate = 0.5;
+//     //     // console.log("player: ", video);
+//     //     // console.log("playback: ", video.playbackRate);
+//     //     // console.log("controls: ", video.controls);
+//     //     // if (video) {
+//     //     //     video.playbackRate = 2;
+//     //     //     console.log("Found video")
+//     //     //     console.log("playback: ", video.playbackRate);
+//     //     // } else {
+//     //     //     console.log("video not found");
+//     //     // }
+//     // }
+
+//     // function initializeDoc(document) {
+//     //     console.log("Website is now loaded!")
+//     //     console.log(document);
+//     // }
+// })();
