1307a67da43b447d06ce08b4a2f342c50549c7c3
Jon Wakefield <jonwakefield.mi@gmail.com>
2024-06-22 13:23:08 -0600

    preliminary support for hbo-max

--- contentScript.js
+++ contentScript.js
@@ -1,14 +1,27 @@
 const ADVIDEOSPEED = 16 // Ad speed
+const defaultVideoSpeed = 1
 const ADSKIPINTERVAL = 500 // units: ms
-const adSkipButtonClassName = ".ytp-skip-ad-button";
+const adSkipButtonClassName = ".ytp-skip-ad-button";0
 const adSkipButtonOldYoutubeUi = ".ytp-ad-skip-button-icon-modern";
+const youtubeAdBanner = '.ytp-ad-duration-remaining';
+const hboAdBanner = ".AdBadgeContainer-Beam-Web-Ent__sc-1jahjvv-1";
 
+/* 
+    HBO TO-DO:
+    1) return UI / Video display back to "no-ad" version
+    2) set video playback speed back to users speed (1x Max doesn't have UI option to change speed)
+
+*/
 
 function domChangeListener(mutationsList, observer) {
     mutationsList.forEach(mutation => {
-      adRemovalProccess();
+        console.log("mutation observered, Checking for ad...")
     });
   }
+
+function findVideoElement() {
+    return document.querySelector('video');
+}
   
 chrome.runtime.onMessage.addListener((obj, sender, response) => {
     const { type, tab } = obj;
@@ -22,9 +35,59 @@ chrome.runtime.onMessage.addListener((obj, sender, response) => {
         } else {
         console.log("No <video> element found on the page.");
         }
+    } else if (type === "HBO") {
+        // set up MutationObserver to detect changes on the DOM, ultimitaly waiting for the video element to load
+        const observer = new MutationObserver((mutationsList, observer) => {
+            for (const mutation of mutationsList) {
+                if (mutation.type === 'childList') {
+                    const adBanner = checkIfAd(hboAdBanner)
+                    if (adBanner) {
+                        console.log("FOUND AD!!!")
+                        // select video element
+                        const videoElement = findVideoElement();
+                        if (videoElement) {
+                            console.log("Selected video element")
+                            let speedChange = changeVideoSpeed(ADVIDEOSPEED);
+                            if (!speedChange) {
+                                console.log("Could not change speed of ad...")
+                                return false
+                            }
+
+                        } else {
+                            console.log("Could not find video element")
+                        }
+
+                        // observer.disconnect();
+                        break;
+                    } else {
+                        const videoElement = findVideoElement();
+                        if (videoElement) {
+                            console.log("Selected video element")
+                            let speedChange = changeVideoSpeed(defaultVideoSpeed);
+                            if (!speedChange) {
+                                console.log("Could not change speed of ad...")
+                                return false
+                            }
+                        }
+
+                    }
+                    // const videoElement = findVideoElement();
+                    // if (videoElement) {
+                    //     console.log("Found video element!")
+                    //     const videoObserver = new MutationObserver(domChangeListener);
+                    //     videoObserver.observe(videoElement, {attributes: true, childList: true, subtree: true });
+
+                    //     // get rid of observer now:
+                    //     observer.disconnect();
+                    //     break;
+                    // }
+                }
+            }
+        });
+        observer.observe(document.body, {childList: true, subtree: true});
     }
 });
-  
+
 
 function adRemovalProccess() {
     /* once a video has been found on the screen, this function is called and starts the ad "removal" process */
@@ -51,11 +114,10 @@ function adRemovalProccess() {
     return false;
 }
 
-function checkIfAd() {
+function checkIfAd(adBanner) {
     /* Function checks if an ad is present on the DOM */
-
     // span element only present if ad is present
-    const adSpanElement = document.querySelector('.ytp-ad-duration-remaining');
+    const adSpanElement = document.querySelector(adBanner);
     if (adSpanElement) {
         return true;   
     } 
